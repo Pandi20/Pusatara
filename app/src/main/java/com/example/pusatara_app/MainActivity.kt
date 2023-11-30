9710930b897e457da2480ce813e1f23adf16122f
carissa-chandra <carissa.tjan03@gmail.com>
2023-11-30 16:18:46 +0700

    Change bottom bar icons

--- app/src/main/java/com/example/pusatara_app/MainActivity.kt
+++ app/src/main/java/com/example/pusatara_app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.pusatara_app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.pusatara_app.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,12 +16,6 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView: BottomNavigationView = binding.bottomNavigation
         val navController = findNavController(R.id.nav_host_fragment)
-
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.menuHome,
-            R.id.menuGlossary,
-            R.id.menuMedia
-        ).build()
 
         bottomNavigationView.setupWithNavController(navController)
     }
