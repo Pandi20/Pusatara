48aaf6873490d10f13a2d6d026671eb86f0fadf4
carissa-chandra <carissa.tjan03@gmail.com>
2023-12-06 16:09:35 +0700

    add toolbar in fragment

--- app/src/main/java/com/example/pusatara_app/MainActivity.kt
+++ app/src/main/java/com/example/pusatara_app/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.pusatara_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.pusatara_app.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,6 +20,14 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView: BottomNavigationView = binding.bottomNavigation
         val navController = findNavController(R.id.nav_host_fragment)
 
+        val toolbar: Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.menuHome, R.id.menuGlossary, R.id.menuMedia
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/pusatara_app/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/pusatara_app/view/home/HomeFragment.kt
@@ -3,12 +3,18 @@ package com.example.pusatara_app.view.home
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.pusatara_app.R
 import com.example.pusatara_app.databinding.FragmentHomeBinding
 import com.example.pusatara_app.view.profile.ProfileActivity
 
+@Suppress("DEPRECATION")
 class HomeFragment : Fragment() {
     private lateinit var binding : FragmentHomeBinding
 
@@ -23,14 +29,39 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.profileHome.setOnClickListener {
-             val intent = Intent(requireActivity(), ProfileActivity::class.java)
-             startActivity(intent)
+        val mainActivity = activity as AppCompatActivity
+        val toolbar = mainActivity.supportActionBar?.apply {
+            title = "Home"
         }
+        setHasOptionsMenu(true)
 
         binding.fabScan.setOnClickListener {
             val intent = Intent(requireActivity(), UploadScanActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.option_menu_home, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search_btn -> {
+                // Handle the settings action
+                // Example: show a toast
+                // Toast.makeText(requireContext(), "Settings clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.profile_btn -> {
+                val intent = Intent(requireActivity(), ProfileActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
